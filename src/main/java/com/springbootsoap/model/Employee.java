1fc566c242ae006e250634cfbbc68fa9b5ff318d
waseem ishaq <wkinglastx@gmail.com>
2024-03-08 12:58:57 +0500

    first commitghjk

---
+++ src/main/java/com/springbootsoap/model/Employee.java
@@ -0,0 +1,22 @@
+package com.springbootsoap.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Employee{
+
+    @Id
+    private Long employeeId;
+
+    private String name;
+
+    private String department;
+
+    private String phone;
+
+    private String address;
+}
